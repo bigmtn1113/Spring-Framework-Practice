6c093a84a704c11a20001e13f03b28a0954210d1
kva23 <kva23@LAPTOP-N8GBEPI1>
2021-01-28 18:37:09 +0900

    Create MVC Project

---
+++ WebContent/WEB-INF/views/include/menu.jsp
@@ -0,0 +1,7 @@
+<%@ page contentType="text/html; charset=UTF-8"%>
+
+<div class="menu">
+	<ul>
+		<li class="chapter">Ch01. <a href="<%=request.getContextPath()%>/ch01/content">Development Environment</a></li>
+	</ul>
+</div>
---
+++ src/main/java/com/mycompany/webapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mycompany.webapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HomeController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("실행");
+	}
+
+}
